a96ebb49a1890d7df656d37de05d57be679d04e2
kronosGR <geo.elgeo@gmail.com>
2021-11-23 20:43:22 +0100

    Added Modal Div

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals'
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <Fragment>
-      <Header/>
+      <Cart />
+      <Header />
       <main>
         <Meals />
       </main>
