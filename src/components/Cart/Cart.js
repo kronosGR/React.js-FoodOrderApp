b74c61310006bdfca91d387934f425535694b30f
Georgios Katsanakis <geo.egleo@gmail.com>
2021-11-26 07:12:10 +0100

    remove item from the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,13 +11,13 @@ const Cart = (props) => {
   const totalAmount = `${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = id =>{
-    
-  }
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAddHandler = item => {
-
-  }
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem(item);
+  };
 
   const cartItems = (
     <ul className={classes['cart-items']}>
@@ -27,8 +27,8 @@ const Cart = (props) => {
           name={item.name}
           amount={item.amount}
           price={item.price}
-          onRemove = {cartItemRemoveHandler.bind(null, item.id)}
-          onAdd= {cartItemAddHandler.bind(null, item)}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
         />
       ))}
     </ul>
