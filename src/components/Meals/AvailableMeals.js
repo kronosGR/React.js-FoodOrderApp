da4a5a0f5abbeb57e269f2c2f9e70c824cb8366e
Georgios Katsanakis <geo.elgeo@gmail.com>
2021-12-02 09:56:27 +0100

    added loading handler

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,6 +6,7 @@ import classes from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     // it is recommended inside useEffect hook
@@ -27,11 +28,17 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
 
+  if (isLoading){
+    return <section className={classes.MealsLoading}>
+      <p>Loading...</p>
+    </section>
+  }
   const mealsList = meals.map((meal) => {
     return (
       <MealItem
